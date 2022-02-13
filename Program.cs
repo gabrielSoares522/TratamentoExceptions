615d60411dd119df5a20acabba7bc8ce73e6f680
gabrielSoares522 <gabrielcsoares52@gmail.com>
2022-02-13 14:08:58 -0300

    criando app para tratar exceptions

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 100,b = 0;
+double resultado = 0;
+
+try{
+    resultado = Dividir(a,b);
+    Console.WriteLine("{0} / {1} = {2}",a,b,resultado);
+}
+catch(DivideByZeroException ex){
+    Console.WriteLine(ex.Message);
+}
+catch (Exception ex){
+    Console.WriteLine(ex.Message);
+    throw;
+}
+finally{
+    Console.WriteLine("Divisão finalizada.");
+}
+
+static double Dividir(int x,int y){
+    if(y == 0) {
+        throw new DivideByZeroException();
+    }
+    return (x/y);
+}
